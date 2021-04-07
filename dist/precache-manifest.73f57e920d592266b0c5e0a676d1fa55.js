39af0c251ebdc359a220af883482067e4ea0559e
Cameron Chambers <ctchambers@oakland.edu>
2021-04-07 10:54:09 -0400

    Added dist folder for GitHub pages

---
+++ dist/precache-manifest.73f57e920d592266b0c5e0a676d1fa55.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cf64983d8dbd5da5c91",
+    "url": "/JavascriptDemos/dist/css/app.9c02cf18.css"
+  },
+  {
+    "revision": "e1187464b762008e16cf",
+    "url": "/JavascriptDemos/dist/css/chunk-vendors.09b7d566.css"
+  },
+  {
+    "revision": "4a97bd76baedc6a0812bff89d717cf32",
+    "url": "/JavascriptDemos/dist/img/black_bishop.4a97bd76.png"
+  },
+  {
+    "revision": "d7cb876357e95fdec4b97316da1d679b",
+    "url": "/JavascriptDemos/dist/img/black_king.d7cb8763.png"
+  },
+  {
+    "revision": "30d1208abcb48677b18a79c2f786d97e",
+    "url": "/JavascriptDemos/dist/img/black_knight.30d1208a.png"
+  },
+  {
+    "revision": "41d82a9417c98fb7c2148ec2e7ae0988",
+    "url": "/JavascriptDemos/dist/img/black_pawn.41d82a94.png"
+  },
+  {
+    "revision": "e57aff201f88f7cfe912b87dc6fb8d33",
+    "url": "/JavascriptDemos/dist/img/black_queen.e57aff20.png"
+  },
+  {
+    "revision": "019b24325d8b4c358de705745d48eff2",
+    "url": "/JavascriptDemos/dist/img/black_rook.019b2432.png"
+  },
+  {
+    "revision": "ea87fdb68768a84b6472c9dc0d208cc0",
+    "url": "/JavascriptDemos/dist/img/white_bishop.ea87fdb6.png"
+  },
+  {
+    "revision": "33ff55999e284ab057f2295340298f47",
+    "url": "/JavascriptDemos/dist/img/white_king.33ff5599.png"
+  },
+  {
+    "revision": "321097b92f643119bc944e3a7fd91505",
+    "url": "/JavascriptDemos/dist/img/white_knight.321097b9.png"
+  },
+  {
+    "revision": "6bc087c31241662faaf6fcb45184f1ad",
+    "url": "/JavascriptDemos/dist/img/white_pawn.6bc087c3.png"
+  },
+  {
+    "revision": "ac545044f625123bb45c136ae51e6381",
+    "url": "/JavascriptDemos/dist/img/white_queen.ac545044.png"
+  },
+  {
+    "revision": "5abb68d32041021dedc93a39c5498adb",
+    "url": "/JavascriptDemos/dist/img/white_rook.5abb68d3.png"
+  },
+  {
+    "revision": "bc73798d50804d88a1b08d4ddc54643d",
+    "url": "/JavascriptDemos/dist/index.html"
+  },
+  {
+    "revision": "5cf64983d8dbd5da5c91",
+    "url": "/JavascriptDemos/dist/js/app.02fbca02.js"
+  },
+  {
+    "revision": "e1187464b762008e16cf",
+    "url": "/JavascriptDemos/dist/js/chunk-vendors.11861337.js"
+  },
+  {
+    "revision": "a83568ad88d48a5ec00591ddc1070d73",
+    "url": "/JavascriptDemos/dist/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/JavascriptDemos/dist/robots.txt"
+  }
+]);
